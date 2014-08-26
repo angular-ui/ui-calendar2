33c1ff6f333fedf6eb3a09f7e0e856a632f0bb82
Mike Tierney <mike@panpainter.com>
2014-08-26 16:39:58 -0700

    chore(tests): Add test harness.

--- src/ui-calendar2.js
+++ src/ui-calendar2.js
@@ -1,1 +1,8 @@
+/* globals angular */
 'use strict';
+
+angular.module('ui.calendar2', [])
+  .constant('uiCalendar2Config', {})
+  .controller('uiCalendar2Ctrl', ['$scope', function ($scope) {
+    return $scope;
+  }]);
